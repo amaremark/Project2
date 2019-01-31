d66c6bd5abb75f5c023d613a4e6d7dea1f30bcb4
Amare AbebeE <amaremark@gmail.com>
2019-01-31 16:39:43 -0500

    first remot commit

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 const express = require("express");
 const hbs = require("hbs");
-//var PORT = process.env || 5000;
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 
 const app = express();
+
 hbs.registerPartials(__dirname + "/views/partials");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,7 +15,7 @@ app.use(methodOverride("_method"));
 
 app.use(require("./routes/index.js"));
 
-
+const port = process.env || 5000;
 
 
 app.listen(5000, () => console.log("server is running"));
