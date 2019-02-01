049764e446484acab0f15d4aa6c6b3977830f1d0
Amare AbebeE <amaremark@gmail.com>
2019-02-01 11:14:16 -0500

    add connecrtor

--- db/connection.js
+++ db/connection.js
@@ -2,4 +2,8 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/recipes");
 mongoose.Promise = Promise;
 module.exports = mongoose;
-
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect("mongodb://localhost/whenpresident");
+}
