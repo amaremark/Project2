001c42609863b4097cfce8662e77a2d0bd378e09
Amare AbebeE <amaremark@gmail.com>
2019-02-01 10:22:42 -0500

    workin on it

--- index.js
+++ index.js
@@ -14,8 +14,11 @@ app.set("view engine", "hbs");
 app.use(methodOverride("_method"));
 
 app.use(require("./routes/index.js"));
+app.set('port, process.env.PORT || 5000')
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
+app.listen(app.get('port'), ()=> {
+    console.log(`PORT: ${app.get('port')}`)
+})
 
-
-app.listen(5000, () => console.log("server is running"));
+// app.listen(5000, () => console.log("server is running"));
